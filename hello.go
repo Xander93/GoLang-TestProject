cd1a8f8e1845f83a4292b299beccc965c1b3bb44
Xander93 <Xdg1993@gmail.com>
2022-01-07 14:07:38 +0100

    playing with imports, for some reason the last import from a local file is not working

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
 import "fmt"
+import "rsc.io/quote"
+import "morestrings/morestrings.go"
 
 func main() {
     fmt.Println("Hello, World!")
+    fmt.Println(quote.Go())
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
